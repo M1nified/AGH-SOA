6412e89225d81effa6db01b6a40b01a8bfd8fe7d
M!nified <M1nified@users.noreply.github.com>
2017-03-06 19:31:49 +0100

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Circus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Circus : ICircus
+    {
+        public List<Animal> Animals;
+        string Name;
+
+        public string AnimalsIntroduction()
+        {
+            string sounds = "";
+            Animals.ForEach(delegate(Animal animal)
+            {
+                sounds += animal.Sound();
+            });
+            return sounds;
+        }
+        public string Show()
+        {
+            string tricks = "";
+            Animals.ForEach(delegate(Animal animal)
+            {
+                tricks += animal.Trick();
+            });
+            return tricks;
+        }
+        public int Patter(int howMuch)
+        {
+            int count = 0;
+            Animals.ForEach(delegate(Animal animal)
+            {
+                count += animal.CountLegs();
+            });
+            return count * howMuch;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Zoo zoo1 = new Zoo();
+            zoo1.Name = "Zoo 1";
+            Elephant elephant1 = new Elephant();
+            elephant1.Name = "Slon 1";
+            Ant ant1 = new Ant();
+            Pony pony1 = new Pony();
+            zoo1.Animals.Add(elephant1);
+            zoo1.Animals.Add(ant1);
+            zoo1.Animals.Add(pony1);
+
+            Console.WriteLine("a) Prezentacja zwierzat w cyrku");
+            Console.WriteLine("b) Program");
+            Console.WriteLine("c) Dzwieki");
+            Console.WriteLine("d) Imie pierwszego");
+            Console.WriteLine("e) Wszystkie imiona");
+
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            Circus circus1 = new Circus();
+            switch (key.Key)
+            {
+                case ConsoleKey.A:
+                    Console.Write(circus1.AnimalsIntroduction());
+            }
+
+        }
+    }
+}
