08336c423b844889765c18e6f1d7dbc312aa6e1c
M!nified <M1nified@users.noreply.github.com>
2017-03-20 18:55:57 +0100

    Zadania3

---
+++ Zadania3/ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [DataContract]
+    class SpaceSystem
+    {
+        [DataMember]
+        public string Name;
+        private int MinShipPower;
+        [DataMember]
+        public int BaseDistance;
+        private int Gold;
+    }
+}
